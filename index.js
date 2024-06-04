7db38a348fe8c14f751b5cfd2d1485fe01e9f1a7
Sahil Kumar <sahil511kumar@gmail.com>
2024-06-04 20:28:29 +0530

    add logic
    Add required functions to play the game

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissor"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
@@ -7,9 +9,56 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   const userInput = prompt("rock, paper or scissor, what's your choice ?");
-  console.log(userInput);
+  return userInput;
 }
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+  const humanChoiceLowerCased = humanChoice?.toLowerCase();
 
-getHumanChoice();
+  if (!choices.includes(humanChoiceLowerCased)) {
+    alert("Invalid value");
+    return;
+  }
+  if (humanChoiceLowerCased === computerChoice) {
+    console.log("Oops, It's a draw");
+  } else if (humanChoiceLowerCased === "rock") {
+    if (computerChoice === "paper") {
+      computerScore += 1;
+      console.log("You Lose! Paper beats Rock");
+    } else if (computerChoice === "scissor") {
+      humanScore += 1;
+      console.log("You Won! Rock beats scissor");
+    }
+  } else if (humanChoiceLowerCased === "paper") {
+    if (computerChoice === "rock") {
+      humanScore += 1;
+      console.log("You Won! Paper beats Rock");
+    } else if (computerChoice === "scissor") {
+      computerScore += 1;
+      console.log("You Lose! Paper beats Scissor");
+    }
+  } else if (humanChoiceLowerCased === "scissor") {
+    if (computerChoice === "paper") {
+      humanScore += 1;
+      console.log("You Won! Scissor beats Paper");
+    } else if (computerChoice === "rock") {
+      computerScore += 1;
+      console.log("You Lose! Rock beats scissor");
+    }
+  }
+}
+
+const playGame = () => {
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+};
+
+playGame();
+
+console.table({
+  "Human Score": humanScore,
+  "Computer Score": computerScore,
+});
