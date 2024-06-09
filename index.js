0ba7efdd426954276e873a9f20eea4f43680e986
Sahil Kumar <sahil511kumar@gmail.com>
2024-06-10 01:43:12 +0530

    add new ui and logic
    Added new Ui layout changes,
    and new logic for playing the game.
    "

--- index.js
+++ index.js
@@ -1,73 +1,99 @@
 const choices = ["rock", "paper", "scissor"];
+const WON_TEXT = "You WON!";
+const LOST_TEXT = "You LOSE!";
+const DEFAULT_HEADER_TEXT = "First to 5 Wins, Try Your Luck !";
+
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
+
+const selections = document.querySelector("#selections");
+const restartBtn = document.querySelector(".restart");
+const winnerNode = document.querySelector(".winner");
+const humanScoreNode = document.getElementById("humanScore");
+const compScoreNode = document.querySelector("#computerScore");
+const alert = document.querySelector("#alert");
+
+window.addEventListener(
+  "load",
+  () => {
+    alert.innerHTML = DEFAULT_HEADER_TEXT;
+  },
+  { once: true }
+);
+
+restartBtn.addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  winnerNode.style.visibility = "hidden";
+  humanScoreNode.textContent = humanScore;
+  compScoreNode.textContent = computerScore;
+  alert.textContent = DEFAULT_HEADER_TEXT;
+  restartBtn.style.visibility = "hidden";
+});
+
+selections.addEventListener("click", (e) => {
+  const id = e.target?.id;
+  if (!choices.includes(id)) return;
+  const humanChoice = id;
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
 
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
   return choices[random];
 }
 
-function getHumanChoice() {
-  const userInput = prompt("rock, paper or scissor, what's your choice ?");
-  return userInput;
-}
-
 function playRound(humanChoice, computerChoice) {
-  const humanChoiceLowerCased = humanChoice?.toLowerCase();
+  if (humanScore === 5 || computerScore === 5) return;
 
-  if (!choices.includes(humanChoiceLowerCased)) {
-    alert("Invalid value");
-    return;
-  }
-  if (humanChoiceLowerCased === computerChoice) {
-    console.log("Oops, It's a draw");
-  } else if (humanChoiceLowerCased === "rock") {
+  let resultText = "";
+  if (humanChoice === computerChoice) {
+    resultText = "DRAW";
+  } else if (humanChoice === "rock") {
     if (computerChoice === "paper") {
       computerScore += 1;
-      console.log("You Lose! Paper beats Rock");
+      resultText = LOST_TEXT;
     } else if (computerChoice === "scissor") {
       humanScore += 1;
-      console.log("You Won! Rock beats scissor");
+      resultText = WON_TEXT;
     }
-  } else if (humanChoiceLowerCased === "paper") {
+  } else if (humanChoice === "paper") {
     if (computerChoice === "rock") {
       humanScore += 1;
-      console.log("You Won! Paper beats Rock");
+      resultText = WON_TEXT;
     } else if (computerChoice === "scissor") {
       computerScore += 1;
-      console.log("You Lose! Paper beats Scissor");
+      resultText = LOST_TEXT;
     }
-  } else if (humanChoiceLowerCased === "scissor") {
+  } else if (humanChoice === "scissor") {
     if (computerChoice === "paper") {
       humanScore += 1;
-      console.log("You Won! Scissor beats Paper");
+      resultText = WON_TEXT;
     } else if (computerChoice === "rock") {
       computerScore += 1;
-      console.log("You Lose! Rock beats scissor");
+      resultText = LOST_TEXT;
     }
   }
+
+  afterEffects(resultText);
 }
 
-const playGame = () => {
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
+const afterEffects = (resultText) => {
+  winnerNode.textContent = resultText;
+  winnerNode.style.visibility = "visible";
+  humanScoreNode.textContent = humanScore;
+  compScoreNode.textContent = computerScore;
+
+  if (humanScore === 5) {
+    restartBtn.style.visibility = "visible";
+    winnerNode.innerHTML =
+      "&#x1F389;&#x1F389; Congratulations, You Win  &#x1F389;&#x1F389;";
+  } else if (computerScore === 5) {
+    winnerNode.innerHTML = "OOps you lost";
+    restartBtn.style.visibility = "visible";
   }
 };
 
-playGame();
-
-console.table({
-  "Human Score": humanScore,
-  "Computer Score": computerScore,
-});
-
-const getWinner = () => {
-  if (computerScore === humanScore) return "It's a drow";
-  return computerScore > humanScore ? "Computer Wins" : "You Win";
-};
-
-const winner = getWinner();
-
-console.log("%c", "color:red", winner);
+const displaySelectionEvent = new CustomEvent("displaySelection");
